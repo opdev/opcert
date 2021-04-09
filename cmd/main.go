65db28079d5baaec752317fee8506d62b884c39e
alexandre <adcmenezes@gmail.com>
2021-04-09 17:38:43 -0400

    cleanup and image pull command

--- cmd/main.go
+++ cmd/main.go
@@ -11,19 +11,7 @@ import (
 	"github.com/savaki/jq"
 
 	scapiv1alpha3 "github.com/operator-framework/api/pkg/apis/scorecard/v1alpha3"
-	apimanifests "github.com/operator-framework/api/pkg/manifests"
 )
-
-// This is the custom scorecard test example binary
-// As with the Redhat scorecard test image, the bundle that is under
-// test is expected to be mounted so that tests can inspect the
-// bundle contents as part of their test implementations.
-// The actual test is to be run is named and that name is passed
-// as an argument to this binary.  This argument mechanism allows
-// this binary to run various tests all from within a single
-// test image.
-
-const PodBundleRoot = "/bundle"
 
 func main() {
 	entrypoint := os.Args[1:]
@@ -43,10 +31,6 @@ func main() {
 	// Names of the custom tests which would be passed in the
 	// `operator-sdk` command.
 	switch entrypoint[0] {
-	// case CustomTest1Name:
-	// 	result = CustomTest1(cfg)
-	// case CustomTest2Name:
-	// 	result = CustomTest2(cfg)
 	case IsRHELTest:
 		result = IsRHEL(img)
 	case HasLabelsTest:
@@ -72,8 +56,8 @@ func printValidTests() scapiv1alpha3.TestStatus {
 	result.Suggestions = make([]string, 0)
 
 	str := fmt.Sprintf("Valid tests for this image include: %s %s",
-		CustomTest1Name,
-		CustomTest2Name)
+		IsRHELTest,
+		HasLabelsTest)
 	result.Errors = append(result.Errors, str)
 	return scapiv1alpha3.TestStatus{
 		Results: []scapiv1alpha3.TestResult{result},
@@ -81,42 +65,9 @@ func printValidTests() scapiv1alpha3.TestStatus {
 }
 
 const (
-	CustomTest1Name = "customtest1"
-	CustomTest2Name = "customtest2"
-	IsRHELTest      = "is_rhel"
-	HasLabelsTest   = "has_labels"
+	IsRHELTest    = "is_rhel"
+	HasLabelsTest = "has_labels"
 )
-
-// Define any operator specific custom tests here.
-// CustomTest1 and CustomTest2 are example test functions. Relevant operator specific
-// test logic is to be implemented in similarly.
-
-func CustomTest1(bundle *apimanifests.Bundle) scapiv1alpha3.TestStatus {
-	r := scapiv1alpha3.TestResult{}
-	r.Name = CustomTest1Name
-	r.State = scapiv1alpha3.PassState
-	r.Errors = make([]string, 0)
-	r.Suggestions = make([]string, 0)
-	almExamples := bundle.CSV.GetAnnotations()["alm-examples"]
-	if almExamples == "" {
-		fmt.Println("no alm-examples in the bundle CSV")
-	}
-
-	return wrapResult(r)
-}
-
-func CustomTest2(bundle *apimanifests.Bundle) scapiv1alpha3.TestStatus {
-	r := scapiv1alpha3.TestResult{}
-	r.Name = CustomTest2Name
-	r.State = scapiv1alpha3.PassState
-	r.Errors = make([]string, 0)
-	r.Suggestions = make([]string, 0)
-	almExamples := bundle.CSV.GetAnnotations()["alm-examples"]
-	if almExamples == "" {
-		fmt.Println("no alm-examples in the bundle CSV")
-	}
-	return wrapResult(r)
-}
 
 // Mandatory tests with possible fail results:
 
@@ -182,14 +133,22 @@ func HasLabels(img string) scapiv1alpha3.TestStatus {
 	r.Errors = make([]string, 0)
 	r.Suggestions = make([]string, 0)
 
-	cmd := exec.Command("docker", "inspect", img)
-	cmdOutput := &bytes.Buffer{}
-	cmd.Stdout = cmdOutput
-	err := cmd.Run()
+	cmd := exec.Command("docker", "pull", img)
 
+	err := cmd.Run()
 	if err != nil {
 		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
+
+	cmd = exec.Command("docker", "inspect", img)
+	cmdOutput := &bytes.Buffer{}
+	cmd.Stdout = cmdOutput
+
+	err = cmd.Run()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+
 	labels := []string{"name", "vendor", "version", "release", "summary", "description"}
 	for _, label := range labels {
 		op, _ := jq.Parse(".[0].Config.Labels." + label)
